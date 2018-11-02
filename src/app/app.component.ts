b879eea8b54ece2f708a7db84d6d881a08194e77
Chris Lebon <chris@chris.me>
2018-11-02 16:50:37 +0100

    working on directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'recipes';
+    title = 'recipes';
+    displayList = false;
+    displayRecipes = true;
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.sass']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.sass']
 })
 export class NavbarComponent implements OnInit {
+    @Output() listSelected = new EventEmitter<boolean>();
+    @Output() recipeSelected = new EventEmitter<boolean>();
+    constructor() { }
 
-  constructor() { }
+    ngOnInit() {
+    }
 
-  ngOnInit() {
-  }
+    showList() {
+        this.listSelected.emit(true);
+        this.recipeSelected.emit(false);
+    }
+
+    showRecipes() {
+        this.listSelected.emit(false);
+        this.recipeSelected.emit(true);
+    }
 
 }
